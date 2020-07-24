512c3eccfff6ffa7c89a180e47f9a79e0a08b9b5
Jordan <jordan.broad1@gmail.com>
2020-07-24 16:30:52 +1200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.items = [
+      {
+        id:1,
+        text: "First Item"
+      },
+      {
+        id:2,
+        text: "Second Item"
+      },
+      {
+        id:3,
+        text: "Third Item"
+      }
+    ]
+
+
+
+
+  }
+
+  render(){
+    return (
+      <div className="wrap">
+        <div className="container">
+            <div className="items">
+              {this.items.map(function(item){
+                var checkbox = "todo-checkbox-"+item.id.toString();
+                return(
+                  <div className="item" key={item.id}>
+                      <label className="text" htmlFor="todo-checkbox-{item.id.toString()}">{item.text}</label>
+                      <input type="checkbox" name="todo-checkbox" id="todo-checkbox-1"/>
+                  </div>
+                )
+              })}
+            </div>
+            
+
+            <div className="newItem">
+                <input type="text" name="new-item" id="new-item"/>
+                <button className="btn">Add</button>
+            </div>
+
+        </div>
     </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
